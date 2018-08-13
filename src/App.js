19abf4a2f657a75d5475d7018d569cc245c1a1ce
christian8aguirre <christian8.aguirre@icloud.com>
2018-08-13 18:31:38 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import request from 'superagent'  //npm install --save superagent
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
+      // cities: [{
+      //   id: 1,
+      //   name: 'France',
+      //   coords: {
+      //     lat: 46.227638,
+      //     lng: 2.213749
+      //   },
+      // },{
+      //   id: 2,
+      //   name: 'Canada',
+      //   coords: {
+      //     lat: 56.130366,
+      //     lng: -106.346771
+      //   },
+      // }],
       show: false
     }
   }
 
   showInput = () =>{
     this.setState({
+      
       show: true
     });
   }
@@ -23,6 +39,29 @@ class App extends Component {
       
     }
   }
+
+  getFranceWeather = (e) => {
+    e.preventDefault();
+    const API_URL = `https://api.darksky.net/forecast/b4821cd62a474edc91d688e05c15d8cc/46.227638,2.213749`
+    
+    request
+      .get(API_URL)
+      .then(response => console.log(response))
+    console.log('France');
+    
+  }
+
+  getCanadaWeather = (e) => {
+    e.preventDefault();
+    const API_URL = 'https://api.darksky.net/forecast/b4821cd62a474edc91d688e05c15d8cc/56.130366,-106.346771'
+    
+    request
+      .get(API_URL)
+      .then(response => console.log(response))
+    console.log('CANADA');
+  }
+
+
 
   render() {
     return (
@@ -35,7 +74,8 @@ class App extends Component {
         <div className='grid'>
           <aside className='app__aside'>
             <h1 className='app__title'>All countries</h1>
-            <a href='#' className='app__country'>France</a>
+            <a href='/' onClick={this.getFranceWeather} className='app__country'>France</a>
+            <a href='/' onClick={this.getCanadaWeather} className='app__country'>Canada</a>
              { this.state.show && <input  autoFocus onKeyDown={this.inputElement} type='text' placeholder='Location' className='app__input' />}
           </aside>
           <section className='app__view'>Text</section>
