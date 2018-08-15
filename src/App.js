eb137594c198b1df7db3723e259ac7863f3eb4d1
christian8aguirre <christian8.aguirre@icloud.com>
2018-08-15 10:12:20 -0500

    d31_weather-app warm up

--- src/App.js
+++ src/App.js
@@ -10,22 +10,38 @@ class App extends Component {
     this.currentElement = React.createRef();
 
     this.state = {
-      cities: [{
-        id: 1,
-        name: 'France',
-        coords: {
-          lat: 46.227638,
-          lng: 2.213749
-        },
-      },{
-        id: 2,
-        name: 'Canada',
-        coords: {
-          lat: 56.130366,
-          lng: -106.346771
-        },
-      }],
-      show: false
+      cities: [],
+      show: false,
+      timezone: 'timezone',
+      summary: '...',
+      daily: [{
+        time: '',
+        icon: '',
+        humidity: '',
+        pressure: '',
+        temperature: '',
+        windSpeed: '',
+        temperatureMax:''
+      }], 
+      vissible: false
+    };
+  }
+
+  addCity = (e) =>{
+    //If sm ENTER was pressed
+    const ENTER_KEY = 13;
+    if (e.keyCode === ENTER_KEY) {
+      this.setState({
+        cities: [
+          ...this.state.cities,
+          {
+            id:this.state.length + 1,
+            name: e.target.value
+          }
+        ],
+        show: false,
+      });
+      e.target.value = '';
     }
   }
 
@@ -36,71 +52,116 @@ class App extends Component {
     });
   }
 
-  inputElement = (e) => {
-    if (e.key === 'Enter') {
-      const country = e.target.value;
-      const API_URL_GOOGLE = `https://maps.googleapis.com/maps/api/geocode/json?address=${country}`
-      e.target.value = '';
-      request
-      .get(API_URL_GOOGLE)
-      .then(res => {
-        const country = res.body.results[0].formatted_address.split(',').slice(-1).join();
-        const lat = res.body.results[0].geometry.location.lat;
-        const lng = res.body.results[0].geometry.location.lng;
-
-          const arrayTemp = [...this.state.cities];
-          arrayTemp.push({
-            id: this.state.cities.length,
-            name: country,
-            coords:{
-              lat: lat,
-              lng: lng
-            }
-          })
-          this.setState({
-            cities: arrayTemp
-          })
-        })
-      }
-      
+  getCoords = (ENDPOINT) => {
+    return request.get(ENDPOINT)
   }
+
+  fetchWeather = (coords) =>{
+    const COORDS = coords.body.results[0].geometry.location;
+    
+    const ENDPOINT = `https://api.darksky.net/forecast/${API_KEY_DARK_SKY}/${COORDS.lat},${COORDS.lng}`
+    
+    request
+      .get(ENDPOINT)
+      .then(res => {
+        console.log(res.body);
+        
+        this.setState({
+          timezone: res.body.timezone,
+          summary: res.body.currently.summary,
+          daily: res.body.daily.data,
+          vissible: true
+        });
+      });
+  }
+
+  fetchLocation = (e) =>{
+    e.preventDefault();
+    const  COUNTRY = e.target.textContent
+    
+    const ENDPOINT = `https://maps.googleapis.com/maps/api/geocode/json?address=${COUNTRY}`
+    
+    this.getCoords(ENDPOINT)
+      .then(this.fetchWeather)
+      .catch(error =>{
+        this.setState({
+          timezone: 'Timezone',
+          summary: 'Sorry man'
+        })
+      })
+    }
+
+  // inputElement = (e) => {
+  //   if (e.key === 'Enter') {
+  //     const country = e.target.value;
+  //     const API_URL_GOOGLE = `https://maps.googleapis.com/maps/api/geocode/json?address=${country}`
+  //     e.target.value = '';
+  //     request
+  //     .get(API_URL_GOOGLE)
+  //     .then(res => {
+  //       const country = res.body.results[0].formatted_address.split(',').slice(-1).join();
+  //       const lat = res.body.results[0].geometry.location.lat;
+  //       const lng = res.body.results[0].geometry.location.lng;
+
+  //         const arrayTemp = [...this.state.cities];
+  //         arrayTemp.push({
+  //           id: this.state.cities.length,
+  //           name: country,
+  //           coords:{
+  //             lat: lat,
+  //             lng: lng
+  //           }
+  //         })
+  //         this.setState({
+  //           cities: arrayTemp
+  //         })
+  //       })
+  //     }
+      
+  // }
   //=========================================================
   //Obtengo el nombre de ciudad y reviso la posición en el state
-  getWeather = (e) => {
-    e.preventDefault();
-    const city = e.target.innerHTML;
-    const pos = this.positionOfCity(city);
-    this.requestDarkSky(pos);
-  }
+  // getWeather = (e) => {
+  //   e.preventDefault();
+  //   const city = e.target.innerHTML;
+  //   const pos = this.positionOfCity(city);
+  //   this.requestDarkSky(pos);
+  // }
   //=========================================================
   //Con la posición de la ciudad, obtengo las coordenadas y hago el request
   //y agrego los datos en el elemento section.
-  requestDarkSky = (pos) =>{
-    const lat = this.state.cities[pos].coords.lat;
-    const lng = this.state.cities[pos].coords.lng;
-    const API_URL_DARKSKY = `https://api.darksky.net/forecast/${API_KEY_DARK_SKY}/${lat},${lng}`
+  // requestDarkSky = (pos) =>{
+  //   const lat = this.state.cities[pos].coords.lat;
+  //   const lng = this.state.cities[pos].coords.lng;
+  //   const API_URL_DARKSKY = `https://api.darksky.net/forecast/${API_KEY_DARK_SKY}/${lat},${lng}`
     
-    request
-      .get(API_URL_DARKSKY)
-      .then(res => {
-        const template = `
-        <ul>
-          <li>CIUDAD: ${this.state.cities[pos].name}</li>
-          <li>TIMEZONE: ${res.body.timezone}</li>
-          <li>Lat: ${res.body.latitude}</li>
-          <li>Lng: ${res.body.longitude}</li>
-          <li>Summary: ${res.body.currently.summary}</li>
-        `;
+  //   request
+  //     .get(API_URL_DARKSKY)
+  //     .then(res => {
+  //       const template = `
+  //       <ul>
+  //         <li>CIUDAD: ${this.state.cities[pos].name}</li>
+  //         <li>TIMEZONE: ${res.body.timezone}</li>
+  //         <li>Lat: ${res.body.latitude}</li>
+  //         <li>Lng: ${res.body.longitude}</li>
+  //         <li>Summary: ${res.body.currently.summary}</li>
+  //       `;
 
-        this.currentElement.current.innerHTML = template;
-      })
+  //       this.currentElement.current.innerHTML = template;
+  //     })
+  // }
+
+  returnDate = (dateUnix) => {
+    const date = new Date(dateUnix*1000).toLocaleDateString();
+    console.log(date);
+    return date;
   }
 
-  positionOfCity = (name) => {
-    const pos = this.state.cities.map(element => 
-      { return element.name; }).indexOf(name);
-      return pos;
-  }
+  // positionOfCity = (name) => {
+  //   const pos = this.state.cities.map(element => 
+  //     { return element.name; }).indexOf(name);
+  //     return pos;
+  // }
 
   render() {
     return (
@@ -114,12 +175,32 @@ class App extends Component {
           <aside className='app__aside'>
             <h1 className='app__title'>All countries</h1>
             { this.state.cities.map(element => {
-              return (<a href='/' id={this.state.id} onClick={this.getWeather} className='app__country'>{element.name}</a>)
+              return (<a href='/' key={this.state.key} onClick={this.fetchLocation} className='app__country'>{element.name}</a>)
             })}
             
-             { this.state.show && <input  autoFocus onKeyDown={this.inputElement} type='text' placeholder='Location' className='app__input' />}
+             { this.state.show && <input  autoFocus onKeyDown={this.addCity} type='text' placeholder='Location' className='app__input' />}
           </aside>
-          <section ref={this.currentElement} className='app__view'>Text</section>
+          <section className='app__view'>
+            <h2>{this.state.timezone}</h2>
+            <h3>{this.state.summary}</h3>
+            <div className='mainList'>
+            {this.state.vissible && 
+              this.state.daily.map(element=>{
+                return (
+                    <ul className= 'dayList'>
+                      <li>{this.returnDate(element.time)}</li>
+                      <li>{element.icon}</li>
+                      <li>{element.humidity}</li>
+                      <li>{element.pressure}</li>
+                      <li>{element.windSpeed}</li>
+                      <li>{element.temperatureMax}</li>
+                    </ul>
+              )
+                }) 
+            }
+            </div>
+            
+          </section>
         </div>
       </div>
     );
